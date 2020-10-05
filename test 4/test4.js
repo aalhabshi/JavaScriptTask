8e058c771f09bbaaac84ae6bd05be11597666402
aalhabshi <lazy.king.2004@gmail.com>
2020-10-05 20:39:07 +0300

    actually done

--- test 4/test4.js
+++
@@ -1,9 +0,0 @@
-function wordCount( val ){
-    var wom = val.match(/\S+/g);
-    return {
-        charactersNoSpaces : val.replace(/\s+/g, '').length,
-        characters         : val.length,
-        words              : wom ? wom.length : 0,
-        lines              : val.split(/\r*\n/).length
-    };
-}
